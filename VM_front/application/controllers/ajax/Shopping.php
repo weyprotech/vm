6b1463a1ea39a7ca0768e6f7031d70a3c71af3dd
Tim Lin <>
2020-05-12 11:32:36 +0800

    -

---
+++ VM_front/application/controllers/ajax/Shopping.php
@@ -0,0 +1,87 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Shopping extends Ajax_Controller
+{
+    public function __construct()
+    {        
+        parent::__construct();
+        $this->load->model('product/tb_product_model','tb_product_model');
+    }
+
+    public function addtocart()
+	{
+		if($this->input->post('productId'))
+		{
+			$productId = $this->input->post('productId');
+			$quantity = $this->input->post('quantity');
+            $size = $this->input->post('size');
+            $color = $this->input->post('color');
+			
+			$row = $this->tb_products_model->get_product_by_id($productId);
+
+			$data = array(
+				'id' => $row->productId,
+				'qty' => $quantity,
+				'price' => $row->price,
+				'productImg' => $row->productImg
+			);
+			
+			$rowid = $this->cart->insert($data);
+			
+			$result = array(
+                'code' => "200",
+                'contents' => $this->cart->contents(),
+				'quantitytotal' => $this->cart->total_items()
+			);
+			
+			echo json_encode($result);
+			return false;
+		}
+    }
+    
+    public function removecart()
+	{
+		if($this->input->post('rowid'))
+		{
+			$rowid = $this->input->post('rowid');
+			
+			$this->cart->remove($rowid);
+
+			$result = array(
+                'code' => "200",
+                'contents' => $this->cart->contents(),
+				'quantitytotal' => $this->cart->total_items(),
+				'total' => $this->cart->total()
+			);
+			
+			echo json_encode($result);
+			return false;
+		}
+	}
+	
+	public function updatecart()
+	{
+		if($this->input->post('rowid'))
+		{
+			$rowid = $this->input->post('rowid');
+			$qty = $this->input->post('qty');
+
+			$data = array(
+				'rowid' => $rowid,
+				'qty' => $qty
+			);
+			
+			$this->cart->update($data);
+			
+			$result = array(
+                'code' => "200",
+                'contents' => $this->cart->contents(),
+				'quantitytotal' => $this->cart->total_items(),
+				'total' => $this->cart->total()
+			);
+			
+			echo json_encode($result);
+			return false;
+		}	
+    }
+}
--- VM_front/application/views/shared/script/product/_detail_script.php
+++ VM_front/application/views/shared/script/product/_detail_script.php
@@ -1,6 +1,35 @@
 <script>
     $(function() {
-    // 愛心的點擊事件
+        var cart;
+        $(".addtocart").on('click', function() {
+            clearTimeout(cart);
+            cart = setTimeout(function() {
+                var productId = $(this).data('productId');
+                var size = $("#size").val();
+                var color = $("#color").val();
+                var quantity = $("#quantity").val();
+
+                $.post(
+                    '<?=site_url('ajax/shopping/addtocart')?>',
+                    { 
+                        'productId' : productId,
+                        'quantity' : quantity,
+                        'size' : size,
+                        'color' : color
+                    }, 
+                    function (res) {
+                        var contact = JSON.parse(res);
+
+                        if(contact.code.trim() == "200")
+                        {
+                            
+                        }
+                    }
+                );
+            }, 1500);
+        });
+
+        // 愛心的點擊事件
         $(document).on('click', '.btn_favorite', function(event) {
             // productId 取得設計師的 ID
             var productId = $(this).attr('data-productId');
